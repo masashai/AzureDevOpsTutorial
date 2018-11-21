5b99caf5602021c6738d2d83da54f9b8bbbb5de9
政史 前田 <mamaeda@tech.softbank.co.jp>
2018-11-21 10:41:16 +0900

    Create Projects

---
+++ AspNetCore.Web/AspNetCore.Answer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCore.Answer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
